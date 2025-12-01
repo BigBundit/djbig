3ac0b7a5b3561f9c4f13a7e020789145304a4342
bigbundit <bigbundit@gmail.com>
2025-12-01 15:45:08 +0700

    update privacy.html

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.djbig',
+  appId: 'com.djbig.djbig',
   appName: 'DJBIG',
   webDir: 'dist'
 };
